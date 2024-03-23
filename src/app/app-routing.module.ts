e8748e30fbaa3f0195ed7e21fbb62a1356a91112
RaffaeleCaravett <136345805+RaffaeleCaravett@users.noreply.github.com>
2024-03-23 21:14:46 +0100

    added control on route

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -2,6 +2,7 @@ import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 import { HomeComponent } from './components/home/home.component';
 import { LoginComponent } from './components/login/login.component';
+import { AuthGuard } from './core/auth.guard';
 
 const routes: Routes = [
   {
@@ -10,7 +11,8 @@ const routes: Routes = [
   },
   {
     path:'office',
-    loadChildren: () => import('./components/office/office.module').then(m => m.OfficeModule)
+    loadChildren: () => import('./components/office/office.module').then(m => m.OfficeModule),
+    canActivate:[AuthGuard]
   },
   {
     path:'login',
