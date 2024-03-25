6ea25ec7e145c8726e5e84ebc659bee068e289ee
RaffaeleCaravett <136345805+RaffaeleCaravett@users.noreply.github.com>
2024-03-25 21:04:02 +0100

    modified office service

--- src/app/service/office.service.ts
+++ src/app/service/office.service.ts
@@ -11,6 +11,7 @@ export class OfficeService{
 
 private auth:string='/auth'
 private categorie:string='/categorie'
+private richiesteTalk:string='/richiesteTalk'
 
 constructor(private http:HttpClient){}
 
@@ -20,4 +21,13 @@ getAllCategories(){
   return this.http.get(environment.API_URL+this.auth+this.categorie)
 }
 
+getAllRichieste(){
+  return this.http.get(environment.API_URL+this.richiesteTalk)
+}
+leggi(richiestaId:number){
+  return this.http.get(environment.API_URL+this.richiesteTalk+`/leggi/${richiestaId}`)
+}
+getByCategoria(categoria:string){
+return this.http.get(environment.API_URL+this.richiesteTalk+`/categoria?categoria=${categoria||'Fitness'}`)
+}
 }
