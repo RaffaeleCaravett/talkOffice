d9b5f2fbd24b117efab02d414dcbb9bdf79c3e01
RaffaeleCaravett <136345805+RaffaeleCaravett@users.noreply.github.com>
2024-03-25 15:05:51 +0100

    succesfuly made autologin

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,44 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { AuthService } from './service/auth.service';
+import { Router } from '@angular/router';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.scss']
 })
-export class AppComponent {
+export class AppComponent implements OnInit{
   title = 'talkOffice';
+
+constructor(private authService:AuthService,private router:Router){}
+
+  ngOnInit(): void {
+      if(localStorage.getItem('accessToken')){
+this.authService.verifyToken(localStorage.getItem('accessToken')!).subscribe({
+  next:(success:any)=>{
+  localStorage.setItem('user',JSON.stringify(success))
+    this.authService.authenticateUser(true)
+    this.router.navigate(['/office'])
+  },
+  error:(err:any)=>{
+    if(localStorage.getItem('refreshToken')){
+      this.authService.verifyRefreshToken(localStorage.getItem('refreshtoken')!).subscribe({
+        next:(success:any)=>{
+          localStorage.setItem('accessToken',success.tokens.accessToken)
+          localStorage.setItem('refreshToken',success.tokens.refreshToken)
+          this.authService.token=localStorage.getItem('accessToken')!
+          this.authService.verifyToken(this.authService.token).subscribe({
+            next:(succes:any)=>{
+              localStorage.setItem('user',JSON.stringify(success))
+              this.authService.authenticateUser(true)
+              this.router.navigate(['/office'])
+            }
+          })
+        }
+      })
+    }
+  }
+})
+      }
+  }
 }
